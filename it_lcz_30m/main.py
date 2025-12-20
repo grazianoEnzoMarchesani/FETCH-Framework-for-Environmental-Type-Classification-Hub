370eccc1b325810e9a2267be89b8caa558586aa6
Il tuo nome <la-tua-email@example.com>
2025-12-20 16:02:19 +0100

    Initialize V2 QGIS Plugin boilerplate (IT-LCZ 30m)

---
+++ it_lcz_30m/main.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtGui import QIcon
+import os.path
+
+class ITLCZ30m:
+    def __init__(self, iface):
+        self.iface = iface
+        self.plugin_dir = os.path.dirname(__file__)
+        self.actions = []
+        self.menu = 'IT-LCZ 30m'
+
+    def tr(self, message):
+        return QCoreApplication.translate('ITLCZ30m', message)
+
+    def add_action(
+        self,
+        icon_path,
+        text,
+        callback,
+        enabled_flag=True,
+        add_to_menu=True,
+        add_to_toolbar=True,
+        status_tip=None,
+        whats_this=None,
+        parent=None):
+
+        icon = QIcon(icon_path)
+        action = QAction(icon, text, parent)
+        action.triggered.connect(callback)
+        action.setEnabled(enabled_flag)
+
+        if status_tip:
+            action.setStatusTip(status_tip)
+        if whats_this:
+            action.setWhatsThis(whats_this)
+
+        if add_to_toolbar:
+            self.iface.addToolBarIcon(action)
+        if add_to_menu:
+            self.iface.addPluginToMenu(self.menu, action)
+
+        self.actions.append(action)
+        return action
+
+    def initGui(self):
+        icon_path = os.path.join(self.plugin_dir, 'icon.png')
+        self.add_action(
+            icon_path,
+            text=self.tr('IT-LCZ 30m Dashboard'),
+            callback=self.run,
+            parent=self.iface.mainWindow()
+        )
+
+    def unload(self):
+        for action in self.actions:
+            self.iface.removePluginMenu(self.menu, action)
+            self.iface.removeToolBarIcon(action)
+
+    def run(self):
+        # This will eventually open the Dashboard UI
+        self.iface.messageBar().pushMessage(
+            "IT-LCZ 30m", "Dashboard in fase di sviluppo!", level=0, duration=5
+        )
